f7376715fa7bb5ae42395556d1fcf7bd5ea3ebe2
Kiel Cruey <kscruey@gmail.com>
2024-03-16 14:27:44 -0700

    added problem_1572 file

---
+++ problem_1572.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+// 1572. Matrix Diagonal Sum
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+
+    }
+};
+
+
+vector<vector<int>> initialize3x3() {
+    vector<int> v1{ 1, 2, 3 };
+    vector<int> v2{ 4, 5, 6 };
+    vector<int> v3{ 7, 8, 9 };
+
+    vector<vector<int>> mat{ v1, v2, v3 };
+
+    return mat;
+}
+
+vector<vector<int>> initialize4x4() {
+    vector<int> v1{ 1, 2, 3, 4 };
+    vector<int> v2{ 5, 6, 7, 8};
+    vector<int> v3{ 9, 10, 11, 12};
+    vector<int> v4{ 13, 14, 15, 16};
+
+    vector<vector<int>> mat{ v1, v2, v3 , v4 };
+
+    return mat;
+}
+
+int main() {
+    vector<vector<int>> mat = initialize3x3();
+
+    Solution s;
+    //auto index = s.diagonalSum(nums);
+
+    return 0;
+}
+
