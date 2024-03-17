c0b9c0258569f0d23c11d7e609cfed9dc5217553
Kiel Cruey <kscruey@gmail.com>
2024-03-17 11:40:21 -0700

    added problem 67

---
+++ problem_67.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+public:
+    string addBinary(string a, string b)
+    {
+
+    }
+};
+
+
+int main() {
+    Solution s;
+
+    std::string a = "1010";
+    std::string b = "1011";
+
+
+    auto results = s.addBinary(a, b);
+
+	return 0;
+}
