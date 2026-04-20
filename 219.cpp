88e6f5fa15191f0572b8a81e42d95de647cff127
Kiel Cruey <kscruey@gmail.com>
2026-04-19 19:25:20 -0700

    wip save

---
+++ 219.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <algorithm> // advanced
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(std::vector<int>& nums, int k) {
+
+        std::vector<int>::iterator begin = nums.begin();
+        std::vector<int>::iterator end = nums.begin();
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i + k < nums.size())
+                std::advance(end, k);
+
+            /*
+            while () {
+
+            }
+            */
+        }
+
+
+        return 0;
+    }
+};
+
+int main() {
+    Solution s;
+
+    std::vector<int> nums{ 1,2,3,1,2,3 };
+
+    s.containsNearbyDuplicate(nums, 2);
+
+    return 0;
+}
