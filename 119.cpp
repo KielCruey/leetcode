3951fbe813ac8f214bc762da263a5d2d49a1774e
Kiel Cruey <kscruey@gmail.com>
2026-05-06 21:57:40 -0700

    solved but takes to long to solve

---
+++ 119.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> generate(int numRows) {
+        std::vector<int> results;
+		int row = numRows + 1; // levels in pascal triangle start at 0
+        
+        int coeff = 1;
+        for (int j = 1; j <= row; j++) {
+            results.push_back(coeff);
+            coeff = coeff * (row - j) / j;
+        }
+
+        return results;
+    }
+};
+
+int main() {
+    Solution solution;
+    std::vector<int> result = solution.generate(3);
+
+    return 0;
+}
