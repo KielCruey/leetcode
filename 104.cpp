64a0346e4c247b4760ff14238077a64d597ed062
Kiel Cruey <kscruey@gmail.com>
2026-07-15 21:43:26 -0700

    complete 104

--- 104.cpp
+++ 104.cpp
@@ -11,76 +11,10 @@ struct TreeNode {
 };
  
 class Solution {
-public:
-    int maxLevel{};
-    std::stack<int> s;
-    
+public:    
     int maxDepth(TreeNode* root) {
-        // use dfs -- using a stack
-        std::stack<TreeNode*> s;
-        int maxLevel{};
+        if (!root) return 0;
 
-        // trivial case
-        if (root == nullptr) return 0;
-
-        s.push(root);
-
-        checkDepth(root, s, maxLevel);
-
-        return maxLevel;
-    }
-
-    void checkDepth(TreeNode* root, std::stack<TreeNode*>& s, int& maxLevel) {
-        while (!s.empty()) {
-            TreeNode* currentNode = root;
-
-            int sTemp = s.size();
-            maxLevel = std::max(maxLevel, sTemp);
-
-            if (root->left != nullptr) {
-                s.push(root->left);
-                checkDepth(root->left, s, maxLevel);
-            } 
-
-            if (root->right != nullptr) {
-                s.push(root->right); 
-                checkDepth(root->right, s, maxLevel);
-            }
-
-            if (!s.empty()) 
-                s.pop();
-        }
+        return std::max(maxDepth(root->left), maxDepth(root->right)) + 1;
     }
 };
-
-int main() {
-    Solution s;
-
-    // test 1
-    /*
-    TreeNode* seven = new TreeNode(7);
-    TreeNode* fifteen = new TreeNode(15);
-    TreeNode* twenty = new TreeNode(20, fifteen, seven);
-    TreeNode* nine = new TreeNode(9);
-    TreeNode* root = new TreeNode(3, nine, twenty);
-    */
-
-    // test 2
-    /*
-    TreeNode* two = new TreeNode(2);
-    TreeNode* root = new TreeNode(1, nullptr, two);
-    */
-
-    // test 3
-    TreeNode* six = new TreeNode(6);
-    TreeNode* five = new TreeNode(5, nullptr, six);
-    TreeNode* four = new TreeNode(4, nullptr, five);
-    TreeNode* three = new TreeNode(3);
-    TreeNode* two = new TreeNode(2, three, nullptr);
-    TreeNode* root = new TreeNode(1, two, four);
-    
-   
-    int results = s.maxDepth(root);
-
-    return 0;
-}
