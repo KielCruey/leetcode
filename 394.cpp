7b219bd489b148dd8dbb1929caac5f32733b7702
Kiel Cruey <kscruey@gmail.com>
2026-08-05 01:19:03 -0500

    wip save

---
+++ 394.cpp
@@ -0,0 +1,20 @@
+#include <string>
+
+class Solution {
+public:
+    std::string decodeString(std::string s) {
+        std::string r;
+        
+        return r;
+    }
+};
+
+int main() {
+    Solution s;
+
+    std::string s1 = "3[a]2[bc]";
+
+    s.decodeString(s1);
+
+    return 0;
+}
